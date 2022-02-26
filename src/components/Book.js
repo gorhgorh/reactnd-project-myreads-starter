f6a5ad8ba17b51919ba319d1a1c18e15290fa919
Runchi <runchidasemail@gmail.com>
2022-02-26 21:32:40 +0100

    - restructure book to function component

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,29 +2,25 @@ import React from 'react'
 import BookSelector from './BookSelector';
 import PropTypes from 'prop-types';
 
-class Book extends React.Component {
-    render() {
-        if (this.props.book) {
-            return (
-                <div className="book">
-                    <div className="book-top">
-                        {/* check thumbnail of the book before rendering cover */}
-                        {this.props.book.imageLinks ?
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${this.props.book.imageLinks.smallThumbnail})` }}></div>
-                            : <div className="book-cover" style={{ width: 128, height: 192 }}></div>
-                        }
-                        <BookSelector moveBook={this.props.moveBook} book={this.props.book} selection={this.props.selection} />
-                    </div>
-                    <div className="book-title">{this.props.book.title}</div>
-                    {this.props.book.authors && <div className="book-authors">{this.props.book.authors.toString()}</div>}
+const Book = (props) => {
+    if (props.book) {
+        return (
+            <div className="book">
+                <div className="book-top">
+                    {/* check thumbnail of the book before rendering cover */}
+                    {props.book.imageLinks ?
+                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${props.book.imageLinks.smallThumbnail})` }}></div>
+                        : <div className="book-cover" style={{ width: 128, height: 192 }}></div>
+                    }
+                    <BookSelector moveBook={props.moveBook} book={props.book} selection={props.selection} />
                 </div>
-            )
-        } else {
-            return null
-        }
+                <div className="book-title">{props.book.title}</div>
+                {props.book.authors && <div className="book-authors">{props.book.authors.toString()}</div>}
+            </div>
+        )
+    } else {
+        return null
     }
-
-    
 }
 
 Book.propTypes = {
